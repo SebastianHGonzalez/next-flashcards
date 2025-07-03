a617c5a9faf4dce836894d62323fc4073f0254da
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2025-07-03 14:39:01 +0000

    Added flashcard model

---
+++ model/flashcard.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const flashcardDetailsSchema = z.object({
+    id: z.string().uuid(),
+    front: z.string().min(1),
+    frontColor: z.string().regex(/^#([0-9a-fA-F]{6})$/),
+});
+
+export type FlashcardDetails = z.infer<typeof flashcardDetailsSchema>;
+
+export const createFlashcardSchema = flashcardDetailsSchema.omit({ id: true })
+
+export type CreateFlashcard = z.infer<typeof createFlashcardSchema>;
+
+export const updateFlashcardSchema = flashcardDetailsSchema.pick({ id: true }).and(flashcardDetailsSchema.omit({ id: true }).partial());
+
+export type UpdateFlashcard = z.infer<typeof updateFlashcardSchema>;
+
+export const deleteFlashcardSchema = flashcardDetailsSchema.pick({ id: true });
+
+export type DeleteFlashcard = z.infer<typeof deleteFlashcardSchema>;
