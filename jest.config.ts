4511d46a6aa2b88f5f2ded7326e8f6008479eaa6
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2025-07-05 14:03:45 +0000

    Fix next-intl in tests

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,21 @@ const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./jest.setup.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: {
+        jsx: 'react-jsx',
+      },
+      useESM: true,
+    }],
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(next-intl|use-intl)/)',
+  ],
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
 }
 
 export default config
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom'
+
+// Mock next-intl
+jest.mock('next-intl', () => ({
+    useTranslations: () => (key: string) => key,
+    useFormatter: () => ({
+        format: (value: any) => value,
+    }),
+}))
