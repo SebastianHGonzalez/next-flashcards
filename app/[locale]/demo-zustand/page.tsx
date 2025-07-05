ccffc1716b911ba60d047fae67629b701385a281
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2025-07-05 14:00:58 +0000

    Refactor flashcards page

--- app/[locale]/demo-zustand/page.tsx
+++ app/[locale]/demo-zustand/page.tsx
@@ -1,22 +1,8 @@
-"use client";
-
 import { useTranslations } from "next-intl";
-import { SearchInput } from "@/components/common/search-input";
-import { Flashcard } from "@/components/flashcard/flashcard";
-import {
-  FlashList,
-  FlashcardListEmpty,
-  FlashcardListItem,
-  FlashcardListNoMatches,
-} from "@/components/flashcard/flashcard-list";
-import { NewFlashcard } from "@/components/flashcard/new-flashcard";
 import { useFlashcards } from "@/hooks/zustand/useFlashcards";
 import { AppBreadcrumb } from "@/components/common/app-breadcrumb";
-import { Alert, AlertDescription, AlertTitle } from "@/components/common/alert";
-import { AlertCircle } from "lucide-react";
 import { Text } from "@/components/common/text";
-import { ActivityIndicator } from "@/components/common/activity-indicator";
-import { Button } from "@/components/common/button";
+import { FlashcardsPage } from "@/components/flashcard/flashcards-page";
 
 export default function DemoZustand() {
   const store = useFlashcards();
@@ -33,61 +19,7 @@ export default function DemoZustand() {
       </Text>
       <Text variant="subheading">{t("flashcards.subtitle")}</Text>
 
-      <SearchInput
-        className="mt-6"
-        id="text-filter"
-        placeholder={t("common.searchPlaceholder")}
-        value={store.textFilter}
-        onChange={(e) => store.setTextFilter(e.target.value)}
-      />
-
-      {store.error && (
-        <Alert variant="destructive" className="mt-4">
-          <AlertCircle />
-          <AlertTitle>{t("alerts.headsUp")}</AlertTitle>
-          <AlertDescription>{store.error}</AlertDescription>
-        </Alert>
-      )}
-
-      {store.loading && <ActivityIndicator size={150} className="mt-4" />}
-
-      {!store.loading && store.flashcards.length > 0 && (
-        <FlashList className="mt-4">
-          {store.flashcards.map((flashcard) => (
-            <FlashcardListItem key={flashcard.id}>
-              <Flashcard
-                flashcard={flashcard}
-                onUpdate={store.updateFlashcard}
-                onDelete={store.deleteFlashcard}
-              />
-            </FlashcardListItem>
-          ))}
-          <FlashcardListItem>
-            <NewFlashcard onAdd={store.addFlashcard} />
-          </FlashcardListItem>
-        </FlashList>
-      )}
-
-      {!store.loading && !store.flashcardsCount && (
-        <FlashcardListEmpty>
-          <NewFlashcard onAdd={store.addFlashcard} />
-        </FlashcardListEmpty>
-      )}
-
-      {!store.loading &&
-        !!store.flashcardsCount &&
-        store.flashcards.length === 0 &&
-        !!store.textFilter && (
-          <FlashcardListNoMatches>
-            <Button
-              variant="outline"
-              className="w-full"
-              onClick={() => store.setTextFilter("")}
-            >
-              {t("flashcards.clearFilters")}
-            </Button>
-          </FlashcardListNoMatches>
-        )}
+      <FlashcardsPage store={store} />
     </div>
   );
 }
---
+++ components/flashcard/flashcards-page.tsx
@@ -0,0 +1,79 @@
+import { useTranslations } from "next-intl";
+import { SearchInput } from "@/components/common/search-input";
+import { Flashcard } from "@/components/flashcard/flashcard";
+import {
+  FlashList,
+  FlashcardListEmpty,
+  FlashcardListItem,
+  FlashcardListNoMatches,
+} from "@/components/flashcard/flashcard-list";
+import { NewFlashcard } from "@/components/flashcard/new-flashcard";
+import { Alert, AlertDescription, AlertTitle } from "@/components/common/alert";
+import { AlertCircle } from "lucide-react";
+import { ActivityIndicator } from "@/components/common/activity-indicator";
+import { Button } from "@/components/common/button";
+import { FlashcardsStore } from "@/model/flashcard";
+
+export function FlashcardsPage({ store }: { store: FlashcardsStore }) {
+  const t = useTranslations();
+
+  return (
+    <>
+      <SearchInput
+        className="mt-6"
+        id="text-filter"
+        placeholder={t("common.searchPlaceholder")}
+        value={store.textFilter}
+        onChange={(e) => store.setTextFilter(e.target.value)}
+      />
+
+      {store.error && (
+        <Alert variant="destructive" className="mt-4">
+          <AlertCircle />
+          <AlertTitle>{t("alerts.headsUp")}</AlertTitle>
+          <AlertDescription>{store.error}</AlertDescription>
+        </Alert>
+      )}
+
+      {store.loading && <ActivityIndicator size={150} className="mt-4" />}
+
+      {!store.loading && store.flashcards.length > 0 && (
+        <FlashList className="mt-4">
+          {store.flashcards.map((flashcard) => (
+            <FlashcardListItem key={flashcard.id}>
+              <Flashcard
+                flashcard={flashcard}
+                onUpdate={store.updateFlashcard}
+                onDelete={store.deleteFlashcard}
+              />
+            </FlashcardListItem>
+          ))}
+          <FlashcardListItem>
+            <NewFlashcard onAdd={store.addFlashcard} />
+          </FlashcardListItem>
+        </FlashList>
+      )}
+
+      {!store.loading && !store.flashcardsCount && (
+        <FlashcardListEmpty>
+          <NewFlashcard onAdd={store.addFlashcard} />
+        </FlashcardListEmpty>
+      )}
+
+      {!store.loading &&
+        !!store.flashcardsCount &&
+        store.flashcards.length === 0 &&
+        !!store.textFilter && (
+          <FlashcardListNoMatches>
+            <Button
+              variant="outline"
+              className="w-full"
+              onClick={() => store.setTextFilter("")}
+            >
+              {t("flashcards.clearFilters")}
+            </Button>
+          </FlashcardListNoMatches>
+        )}
+    </>
+  );
+}
